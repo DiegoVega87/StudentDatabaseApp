68165908a86fdd9a6aed1741e9c9237938d3e10f
DiegoVega87 <diegovega@me.com>
2024-01-12 18:16:42 +0100

    Add initial menu to add students to the database

--- src/StudentDatabaseApp.java
+++ src/StudentDatabaseApp.java
@@ -1,12 +1,27 @@
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
 
 public class StudentDatabaseApp {
 
     public static void main(String[] args) {
 
+        ArrayList<Student> students = new ArrayList<>();
         Set<String> IDs = new HashSet<>();
-        Student st = new Student("Diego", "Vega", "junior", IDs);
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of students to add to the database: ");
+        int numStudents = input.nextInt();
+        input.nextLine();
+        while(numStudents > 0){
+            System.out.println("Please Enter student's First Name: ");
+            String firstName = input.nextLine();
+            System.out.println("Please Enter student's Last Name: ");
+            String lastName = input.nextLine();
+            System.out.println("Please enter Student's year: ");
+            String year = input.nextLine();
+            //while (!year.equalsIgnoreCase(""))
+        }
 
     }
 }
