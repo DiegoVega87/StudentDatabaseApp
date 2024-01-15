d9098dc4c4d60c3c234e5fb8226dd5a1644b0d86
DiegoVega87 <diegovega@me.com>
2024-01-15 12:42:38 +0100

    Implement to string method and added more funtionality

--- src/Student.java
+++ src/Student.java
@@ -1,7 +1,5 @@
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.Scanner;
-import java.util.Set;
 
 public class Student {
 
@@ -19,29 +17,28 @@ public class Student {
 
     public Student() {
 
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter the number of students to add to the database: ");
-        int numStudents = input.nextInt();
-        input.nextLine();
-        while(numStudents > 0) {
-            System.out.println("Please Enter student's First Name: ");
-            this.firstName = input.nextLine();
-            System.out.println("Please Enter student's Last Name: ");
-            this.lastName = input.nextLine();
-            System.out.println("""
-                    Please enter Student's year:\s
-                    1 - Freshman
-                    2 - Sophomore\s
-                    3 - Junior\s
-                    4 - Senior""");
-            this.gradeYear = input.nextInt();
-            input.nextLine();
+        courses = new ArrayList<>();
+        status = "Not Enrolled";
 
-            while ((this.gradeYear != 1) &&
-                    (this.gradeYear != 2) &&
-                    (this.gradeYear != 3) &&
-                    (this.gradeYear != 4)) {
-                System.out.println("""
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please Enter student's First Name: ");
+        this.firstName = input.nextLine();
+        System.out.print("Please Enter student's Last Name: ");
+        this.lastName = input.nextLine();
+        System.out.println("""
+                Please enter Student's year:\s
+                1 - Freshman
+                2 - Sophomore\s
+                3 - Junior\s
+                4 - Senior""");
+        this.gradeYear = input.nextInt();
+        input.nextLine();
+
+        while ((this.gradeYear != 1) &&
+                (this.gradeYear != 2) &&
+                (this.gradeYear != 3) &&
+                (this.gradeYear != 4)) {
+            System.out.println("""
                     ERROR: Please enter a valid year: \s
                     1 - Freshman
                     2 - Sophomore\s
@@ -49,81 +46,86 @@ public class Student {
                     4 - Senior
                     """);
 
-                this.gradeYear = input.nextInt();
-            }
-            numStudents--;
+            this.gradeYear = input.nextInt();
+
         }
         setStudentID();
-        System.out.println(firstName + " " + lastName + " " + gradeYear + " " + studentID);
 
     }
 
     private void setStudentID() {
         id++;
-        this.studentID = gradeYear + "" +  id;
+        this.studentID = gradeYear + "" + id;
     }
 
-    public boolean addCourse(){
+    public void addCourse() {
         Scanner in = new Scanner(System.in);
         String option = "";
-        System.out.println("""
-                Please choose:\s
-                1 - History 101
-                2 - Mathematics 101
-                3 - English 101
-                4 - Chemistry 101
-                5 - Computer Science 101""");
-        switch (in.nextInt()){
-            case 1 -> option = "History 101";
-            case 2 -> option = "Mathematics 101";
-            case 3 -> option = "English 101";
-            case 4 -> option = "Chemistry 101";
-            case 5 -> option = "Computer Science 101";
-            default -> {System.out.println("Invalid option, please choose from 1-5.");
-            return false;}
+        do {
+            System.out.println("""
+                    Please choose course to enroll:\s
+                    0 - Quit
+                    1 - History 101
+                    2 - Mathematics 101
+                    3 - English 101
+                    4 - Chemistry 101
+                    5 - Computer Science 101""");
+            switch (in.nextInt()) {
+                case 0 -> {
+                    return;
+                }
+                case 1 -> option = "History 101";
+                case 2 -> option = "Mathematics 101";
+                case 3 -> option = "English 101";
+                case 4 -> option = "Chemistry 101";
+                case 5 -> option = "Computer Science 101";
+                default -> System.out.println("Invalid option, please choose from 1-5.");
 
-        }
-        if(courses.size() == 5){
-            System.out.println("ERROR: Student reached max number of courses/term");
-        }else if(courses.contains(option)){
-            System.out.println("ERROR: Student already enrolled for " + option);
-        }else{
-            if(status.equals("Not enrolled")){
-                status = "Enrolled";
             }
-            System.out.println("Enrollment successful!");
-            balance += COURSE_COST;
-            return courses.add(option);
-        }
-        return false;
+
+            if (courses.contains(option)) {
+                System.out.println("ERROR: Student already enrolled for " + option);
+            } else {
+                if (status.equalsIgnoreCase("Not enrolled")) {
+                    status = "Enrolled";
+                }
+                System.out.println("Enrollment successful!");
+                balance += COURSE_COST;
+                courses.add(option);
+            }
+        } while (courses.size() < 5);
+        in.close();
     }
 
-    public int getBalance(){
-        return balance;
+    public void getBalance() {
+        System.out.println("Your balance is : $" + balance);
     }
 
-    public boolean payTuition(int amount){
-        if(amount < balance/2){
-            System.out.println("Payment not processed. Payment must be greater than " + balance/2 + "\n");
-        }else if(amount > balance){
+    public void payTuition() {
+        getBalance();
+        Scanner in = new Scanner(System.in);
+        System.out.print("Please enter amount: $");
+        int amount = in.nextInt();
+        in.close();
+        if (amount < balance / 2) {
+            System.out.println("Payment not processed. Payment must be greater than " + balance / 2 + "\n");
+            getBalance();
+        } else if (amount > balance) {
             System.out.println("Payment not processed. Payment must be less than or equal to " + balance + "\n");
-        }else{
+        } else {
             balance -= amount;
             System.out.println("Your balance is: " + balance);
-            return true;
         }
-        return false;
     }
 
-    public void showInfo(){
-        System.out.println("Student StudentID:" + studentID);
-        System.out.println("First Name: " + firstName + "\nLast Name: " + lastName);
-        System.out.println("Status: " + status);
-        System.out.println("Courses: ");
-        for(String s : courses){
-            System.out.println(s);
-        }
-        System.out.println("Balance: " + balance);
+    public String toString() {
+        return
+                "Student StudentID:" + studentID +
+                        "\nFirst Name: " + firstName + "\nLast Name: " + lastName +
+                        "\nGrade Level: " + gradeYear +
+                        "\nStatus: " + status +
+                        "\nCourses: " + courses +
+                        "\nBalance $" + balance;
     }
 
 }
--- src/StudentDatabaseApp.java
+++ src/StudentDatabaseApp.java
@@ -7,5 +7,8 @@ public class StudentDatabaseApp {
 
     public static void main(String[] args) {
         Student st = new Student();
+        st.addCourse();
+        st.payTuition();
+        System.out.println(st);
     }
 }
